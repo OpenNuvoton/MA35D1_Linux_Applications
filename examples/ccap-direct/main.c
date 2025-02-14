077c90bebd757d644b9f0e1724a65d688974247e
Jacky Huang <ychuang3@nuvoton.com>
2025-02-14 10:26:13 +0800

    example: ccap: Add demo for taking a snapshot image from camera preview
    The snapshot demo requires the LCD overlay resolution to be configured the
    same as the snapshot image size.
    The snapshot image is obtained from CCAP ioctl mmap memory.
    In this demo, the snapshot image is copied to the LCD overlay by the CPU.
    
    Change-Id: Iecc300b387dfd2006b4cf6a66b119bd768a3addf

--- examples/ccap-direct/main.c
+++ examples/ccap-direct/main.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2024 Nuvoton technology corporation
  * All rights reserved.
@@ -18,23 +19,84 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <linux/fb.h>
 #include <linux/videodev2.h>
 
-#define IOCTL_CCAP_FBD_SET	_IOW ('v', 61, struct ccap_fb_direct)
-#define IOCTL_CCAP_FBD_GET	_IOW ('v', 62, struct ccap_fb_direct)
+#define IMAGE_WIDTH		640
+#define IMAGE_HEIGHT		480
+
+#define DO_SNAPSHOT_DEMO	1
+
+#define IOCTL_CCAP_FBD_SET		_IOW('v', 61, struct ccap_fb_direct)
+#define IOCTL_CCAP_FBD_GET		_IOW('v', 62, struct ccap_fb_direct)
+#define IOCTL_CCAP_SNAPSHOT_DO		_IOW('v', 65, int)
+#define IOCTL_CCAP_SNAPSHOT_IDLE	_IOW('v', 66, int)
 
 struct ccap_fb_direct {
-	int enable;      /* 0: disable; 1: enable     */
-	int is_overlay;  /* 0: main frame; 1: overlay */
-	int fb_width;    /* frame buffer width        */
-	int fb_height;   /* frame buffer height       */
-	int img_x;       /* image render left x       */
-	int img_y;       /* image render top y        */
-	int img_width;   /* image width               */
-	int img_height;  /* image height              */
+	int enable;       /* 0: disable; 1: enable     */
+	int wait_sync;    /* wait for LCD VSYNC        */
+	int is_overlay;   /* 0: main frame; 1: overlay */
+	int fb_width;     /* frame buffer width        */
+	int fb_height;    /* frame buffer height       */
+	int img_x;        /* image render left x       */
+	int img_y;        /* image render top y        */
+	int img_width;    /* image width               */
+	int img_height;   /* image height              */
+	int do_snapshot;  /* -1: idle; 0: doing; 1: to do */
 };
 
-void ccap_control()
+struct overlay {
+	int fd;
+	__u8 *memp;
+} _overlay;
+
+int init_overlay(void)
+{
+	struct fb_fix_screeninfo finfo;
+	__u8 *fb_mem, *image;
+	int fd;
+
+	fd = open("/dev/fb1", O_RDWR | O_SYNC);
+	if (fd < 0) {
+		perror("Failed to open fb1\n");
+		return -1;
+	}
+
+	if (ioctl(fd, FBIOGET_FSCREENINFO, &finfo)) {
+		perror("Error reading fixed information");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+
+	_overlay.memp = (__u8 *)mmap(NULL, finfo.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (_overlay.memp == MAP_FAILED) {
+		perror("fb1 mmap failed!!\n");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+
+	memset(_overlay.memp, 0, finfo.smem_len);
+
+	_overlay.fd = fd;
+	return 0;
+}
+
+int do_sanp_shot(int fd)
+{
+	int x = 1, wait_time = 1000;
+
+	ioctl(fd, IOCTL_CCAP_SNAPSHOT_DO, &x);
+	do {
+		usleep(1000);
+		if (wait_time-- <= 0)
+			return -1;
+	} while (ioctl(fd, IOCTL_CCAP_SNAPSHOT_IDLE, &x) != 0);
+	return 0;
+}
+
+void ccap_control(void)
 {
 	struct v4l2_fmtdesc fmt;
 	struct v4l2_frmsizeenum frmsize;
@@ -42,7 +104,7 @@ void ccap_control()
 	struct ccap_fb_direct fb_direct;
 	int fd, x, y;
 
-	fd = open("/dev/video0", O_RDWR | O_NONBLOCK, 0);
+	fd = open("/dev/video0", O_RDWR | O_NONBLOCK | O_SYNC, 0);
 	if (fd < 0) {
 		printf("Failed to open /dev/video0\n");
 		exit(EXIT_FAILURE);
@@ -50,9 +112,8 @@ void ccap_control()
 
 	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	fmt.index = 0;
-	if (ioctl(fd, VIDIOC_ENUM_FMT, &fmt) == 0) {
-    		printf("Pixel Format: %s\n", fmt.description);
-	}
+	if (ioctl(fd, VIDIOC_ENUM_FMT, &fmt) == 0)
+		printf("Pixel Format: %s\n", fmt.description);
 
 	printf("Supported Pixel Formats:\n");
 	for (fmt.index = 0, fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -91,28 +152,56 @@ void ccap_control()
 	}
 
 	fb_direct.enable = 1;
+	fb_direct.wait_sync = 1;
 	fb_direct.is_overlay = 0;     /* fb0 */
 	fb_direct.fb_width = 1024;    /* LCD WIDTH*/
 	fb_direct.fb_height = 600;    /* LCD HEIGHT */
 	fb_direct.img_x = 0;
 	fb_direct.img_y = 0;
-	fb_direct.img_width = 640;    /* capture image width */
-	fb_direct.img_height = 480;   /* capture image height */
+	fb_direct.img_width = IMAGE_WIDTH;
+	fb_direct.img_height = IMAGE_HEIGHT;
 	ioctl(fd, IOCTL_CCAP_FBD_SET, &fb_direct);
 
-	for (x = 0; ; x = (x + 30) % 300) {
-		for (y = 0; y <= 120; y += 30) {
+	if (DO_SNAPSHOT_DEMO) {
+		__u8 *image;
+
+		if (init_overlay() != 0)
+			exit(EXIT_FAILURE);
+
+		/* get ccap memory mapping */
+		image = mmap(NULL, IMAGE_WIDTH * IMAGE_HEIGHT * 4, PROT_READ, MAP_SHARED, fd, 0);
+		if (image == MAP_FAILED || !image) {
+			printf("mmap failed!!\n");
+			close(fd);
+			exit(EXIT_FAILURE);
+		}
+
+		while (1) {
 			sleep(1);
-			fb_direct.img_x = x;
-			fb_direct.img_y = y;
-			ioctl(fd, IOCTL_CCAP_FBD_SET, &fb_direct);
+			if (do_sanp_shot(fd) == 0)
+				memcpy(_overlay.memp, image, IMAGE_WIDTH * IMAGE_HEIGHT * 4);
+			else
+				printf("Failed to catpure images!\n");
+		}
+	} else {
+		for (x = 0; ; x = (x + 30) % 300) {
+			for (y = 0; y <= 120; y += 30) {
+				sleep(1);
+				fb_direct.img_x = x;
+				fb_direct.img_y = y;
+				ioctl(fd, IOCTL_CCAP_FBD_SET, &fb_direct);
+			}
 		}
 	}
 }
 
-void gstreamer_v4l2_capture()
+void gstreamer_v4l2_capture(void)
 {
-	const char gst_command[] = "gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,format=RGB16 ! fakesink";
+	char gst_command[512];
+
+	snprintf(gst_command, sizeof(gst_command),
+		"gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,width=%d,height=%d,format=RGB16 ! fakesink",
+		IMAGE_WIDTH, IMAGE_HEIGHT);
 
 	printf("Starting GStreamer command: %s\n", gst_command);
 
